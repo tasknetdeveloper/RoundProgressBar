bcbea539dfcf2ef36cd5c8c13a303e3361258fa2
Aicos <56368289+tasknetdeveloper@users.noreply.github.com>
2019-10-14 22:28:22 +0300

    Add files via upload
    inserted files of project

---
+++ RoundProgressBar/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RoundProgressBar
+{
+    public partial class Form1 : Form
+    {
+        private CircularProgressBar cbar = new CircularProgressBar();
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        private void Ini()
+        {
+            pictureBox1.Controls.Add(cbar);
+            cbar.RunProgress();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ini();
+        }
+    }
+}
